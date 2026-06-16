f7fcf668588f5c06774ecec7671b3f66f773e86a
Douglas Marquardt <noreply@outlook.com>
2026-06-16 14:12:42 -0600

    Scaffold Summary contents for DDS Method pages

---
+++ src/MainLib/Sandbox/Components/ComponentHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Sandbox.UI.Models;
+
+namespace Sandbox.Components
+{
+    internal static class ComponentHelpers
+    {
+        public static async Task HandleInitializeError(this ILogger logger, Exception ex, IToastService? toastService = null) 
+        {
+            logger.LogError(ex, "An error occurred while initializing this page.");
+
+            if (toastService != null)
+                await toastService.ShowError("An error occurred while initializing this page.");
+        }
+    }
+}
