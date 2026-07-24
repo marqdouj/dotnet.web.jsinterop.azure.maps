7e458be9e058bea5fae56ee2721e0e79655ebdfe
Douglas Marquardt <noreply@outlook.com>
2026-07-24 10:07:43 -0600

    add AppConfiguration.HeaderText

--- src/MainLib/Sandbox/Models/AppConfiguration.cs
+++ src/MainLib/Sandbox/Models/AppConfiguration.cs
@@ -3,5 +3,6 @@
     public class AppConfiguration(WebApplicationBuilder builder)
     {
         public bool IsDevelopment { get; } = builder.Environment.IsDevelopment();
+        public string HeaderText { get; } = "Azure Maps Sandbox";
     }
 }
